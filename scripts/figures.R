d6ed6060ce5131fcc145f35023e99ae4fe9bbbb6
matt <ml16847@bristol.ac.uk>
2021-12-08 19:15:49 +0000

    scripts for figures and tables

---
+++ scripts/figures.R
@@ -0,0 +1,102 @@
+rm(list=ls())
+
+# Packages ====
+library(wesanderson)
+library(ggplot2)
+library(dplyr)
+library(cowplot)
+library(data.table)
+
+# Palettes ====
+d1 <- wes_palette("Royal1", type = "discrete")
+d2 <- wes_palette("GrandBudapest2", type = "discrete")
+d3 <- wes_palette("Cavalcanti1", type = "discrete")
+d4 <- wes_palette("Darjeeling1", type = "discrete")
+discrete_wes_pal <- c(d1, d2, d3, d4)
+continuous_wes_pal <- wes_palette("Zissou1", 100, type = "continuous")
+
+# data ====
+load("../systematic_review_MR_adiposity/analysis/data_extraction.RData")
+
+year <- data[!duplicated(data$ID),]
+year <- year[,c("ID", "year", "outcome_n_case_current", "outcome_n_control_current", "exposure_n_current")]
+year$exposure_n_current <- as.numeric(year$exposure_n_current)
+year$outcome_n_case_current <- as.numeric(year$outcome_n_case_current)
+year$outcome_n_control_current <- as.numeric(year$outcome_n_control_current)
+
+a <- year[,c("year", "exposure_n_current")]
+a <- a[complete.cases(a), ]
+exposure <- a %>% 
+  group_by(year) %>% 
+  summarise(exposure_average = mean(exposure_n_current))
+
+a <- year[,c("year", "outcome_n_case_current", "outcome_n_control_current")]
+a[is.na(a)] <- 0
+a$outcome <- a$outcome_n_case_current + a$outcome_n_control_current
+a <- subset(a, outcome > 0)
+a <- a[,c("year", "outcome")]
+a <- a[complete.cases(a), ]
+outcome <- a %>% 
+  group_by(year) %>% 
+  summarise(outcome_average = mean(outcome))
+
+a <- year[,c("year")]
+a <- a[complete.cases(a), ]
+year <- a %>% 
+  group_by(year) %>%
+  summarise(year_count = length(year))
+
+plot_data <- left_join(year, exposure, by = "year")
+plot_data <- left_join(plot_data, outcome, by = "year")
+
+# plot ====
+plot_data$`N articles` <- plot_data$year_count
+p1 <- ggplot(plot_data) +
+  geom_col(aes(x = year, y = `N articles`)) +
+  geom_point(aes(x = year, y = exposure_average/5000, group = 1), size = 2, colour = discrete_wes_pal[1]) +
+  geom_point(aes(x = year, y = outcome_average/5000, group = 1), size = 2, colour = discrete_wes_pal[2]) +
+  geom_line(aes(x = year, y = exposure_average/5000, group = 1), size = 3, colour = discrete_wes_pal[1]) +
+  geom_line(aes(x = year, y = outcome_average/5000, group = 1), size = 3, colour = discrete_wes_pal[2]) +
+  scale_y_continuous(sec.axis = sec_axis(~.*5000, breaks = c(250000,200000,150000,100000,50000,0), name = "N exposure/outcome")) +
+  theme_cowplot() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+pdf("figures/n_articles.pdf", width = 10, height = 10)
+p1
+dev.off()
+
+design <- select(data, ID, MR_design)
+design <- as.data.table(design)
+design <- dcast(design, MR_design ~ ., fun.agg = function(x) sum(!is.na(x)), 
+                value.var = "ID")
+colnames(design)[2] <- "N"
+design$MR_design <- gsub("one-sample meta-analysis", "One-sample meta-analysis", design$MR_design)
+design$MR_design <- gsub("two-sample meta-analysis", "Two-sample meta-analysis", design$MR_design)
+design$MR_design <- gsub("one-sample and Two-sample meta-analysis", "One-sample and two-sample meta-analysis", design$MR_design)
+p1 <- ggplot(data.frame(design), aes(x=reorder(MR_design, N), y=N)) +
+  geom_bar(stat='identity') +
+  theme_cowplot() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+        axis.text.y = element_text(size = 8),
+        axis.title.y = element_blank(), axis.title.x = element_blank()) +
+  coord_cartesian(xlim=c(0,2000), ylim=c(0,2000)) +
+  # geom_text(aes(label=N), position=position_dodge(width=0.9), vjust=0.5, hjust=-0.5) +
+  coord_flip()
+pdf("figures/study_design.pdf", width = 10, height = 10)
+p1
+dev.off()
+
+# ====
+rob_data <- read.table("../systematic_review_MR_adiposity/analysis/quality_assessment/quality_assessment_results.txt", header = T, sep = "\t")
+rob_data$QA[rob_data$Total <= 19] <- "High"
+rob_data$QA[rob_data$Total >= 20 ] <- "Medium"
+rob_data$QA[rob_data$Total >= 28] <- "Low"
+rob_data$QA <- factor(rob_data$QA, levels = c("High", "Medium", "Low"))
+
+p1 <- ggplot(data = rob_data,
+       aes(Total, fill = QA)) +
+  geom_histogram(binwidth = 1, alpha = 0.8) +
+  theme_cowplot()
+pdf("figures/qa_distribution.pdf", width = 10, height = 10)
+p1
+dev.off()
---
+++ scripts/tables.R
@@ -0,0 +1,50 @@
+rm(list=ls())
+
+# Packages ====
+library(dplyr)
+library(data.table)
+library(kableExtra)
+
+# data ====
+load("../systematic_review_MR_adiposity/analysis/data_extraction.RData")
+
+exposure_percentages$freq <- exposure_percentages$freq * 100
+exposure_percentages$exposure <- gsub("body fat percentage", "BF", exposure_percentages$exposure)
+knitr::kable(
+  (exposure_percentages[order(-exposure_percentages$n),]),
+  longtable = T, booktabs = T,
+  caption = 'Number and frequency of exposures used across all 2,214 Mendelian randomization analyses', row.names = F,
+  digits = 2, format = "latex", 
+  col.names = c("Exposure", "N", "%")) %>%
+  kable_styling(latex_options = c("striped", "HOLD_position", "repeat_header"), position = "center") %>%
+  kableExtra::footnote(general = "BMI = body mass index; WHR = waist hip ratio; WHRadjBMI = WHR adjusted for BMI; WC = waist circumference; WCadjBMI = WC adjusted for BMI; HC = hip circumference; HCadjBMI = HC adjusted for BMI; BF = body fat percentage.",
+                       threeparttable = T, general_title = "", footnote_as_chunk = T)
+
+outcome_percentages$freq <- outcome_percentages$freq * 100
+knitr::kable(
+  head(outcome_percentages[order(-outcome_percentages$n),], 10),
+  longtable = F, booktabs = T,
+  caption = 'Number and frequency of the 10 most used outcomes across all 2214 MR analyses', row.names = F,
+  digits = 2, format = "latex",
+  col.names = c("Outcome", "N", "%")) %>%
+  kable_styling(latex_options = c("striped", "HOLD_position", "repeat_header"), position = "center")
+
+outcome_percentages_group$freq <- outcome_percentages_group$freq * 100
+knitr::kable(
+  (outcome_percentages_group[order(-outcome_percentages_group$n),]),
+  longtable = F, booktabs = T,
+  caption = 'Number and frequency of outcomes within each outcome category across all 2214 Mendelian randomization analyses', row.names = F,
+  digits = 2, format = "latex",
+  col.names = c("Group", "N", "%")) %>%
+  kable_styling(latex_options = c("striped", "HOLD_position", "repeat_header"), position = "center") %>%
+  kableExtra::footnote(general = "Outcome groups were assigned based on prior biological knowledge and aimed to collapse the large number of outcomes. This could be achieved differently for some outcomes, for example smoking could go in a 'respiratory' group or a 'behavioural' group. Where there were few outcomes to make a group, they were grouped into an 'other' group. This will include outcomes such as mortality, disease counts, epigenetic markers, etc.",
+                       threeparttable = T, general_title = "", footnote_as_chunk = T)
+
+studies <- read.table("../systematic_review_MR_adiposity/analysis/meta_analysis/results/study_freq.txt", header = T, sep = "\t")
+knitr::kable(
+  (studies[order(-studies$freq),]),
+  longtable = F, booktabs = T,
+  caption = 'Number of times an article was used in meta-analyses', row.names = F,
+  digits = 2, format = "markdown",
+  col.names = c("Article", "N")) %>%
+  kable_styling(latex_options = c("striped", "HOLD_position", "repeat_header"), position = "center", font_size = 8)
