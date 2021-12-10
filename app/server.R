be6eae7c32408ac7e8ea80fae9cb52f6e2f827fd
matt <ml16847@bristol.ac.uk>
2021-12-10 12:40:38 +0000

    minor update

--- app/server.R
+++ app/server.R
@@ -4,13 +4,13 @@ function(input, output, session) {
   output$data_extraction <- DT::renderDataTable(DT::datatable({
     data_extraction
     if (input$exposure != "All") {
-      data_extraction <- data_extraction[data_extraction$exposure == input$exposure,]
+      data_extraction <- data_extraction[data_extraction$Exposure == input$exposure,]
     }
     if (input$outcome != "All") {
-      data_extraction <- data_extraction[data_extraction$outcome == input$outcome,]
+      data_extraction <- data_extraction[data_extraction$Outcome == input$outcome,]
     }
     if (input$outcome_group != "All") {
-      data_extraction <- data_extraction[data_extraction$outcome_group == input$outcome_group,]
+      data_extraction <- data_extraction[data_extraction$`Outcome group` == input$outcome_group,]
     }
     data_extraction
   }))
